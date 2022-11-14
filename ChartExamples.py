df4bdd0e732f99f1401f5bb6770e241439522378
nutoss <113785849+nutoss@users.noreply.github.com>
2022-11-14 14:53:31 +0300

    Создание основы программы

---
+++ ChartExamples.py
@@ -0,0 +1,13 @@
+
+#Примеры построения графиков
+import tkinter as tk
+
+#Создание главного цикла
+window=tk.Tk()
+window.geometry("450x450")
+window.title ("Примеры построения графиков")
+
+
+#Запуск цикла mainloop
+
+window.mainloop()
