7115d3cd505e9e8cea3e8180c2257e64dc0cef1e
nutoss <113785849+nutoss@users.noreply.github.com>
2022-11-23 13:16:45 +0300

    Добавление графиков

--- ChartExamples.py
+++ ChartExamples.py
@@ -2,12 +2,16 @@
 #Примеры построения графиков
 import tkinter as tk
 
+# Импорт внешних файлов
+import chart1
+import chart2
+
 #Функция закрытия программы
 def do_close():
    window.destroy()
 
 
-#Создание главного цикла
+#Создание главного окна
 window=tk.Tk()
 window.geometry("450x450")
 window.title ("Примеры построения графиков")
@@ -18,11 +22,19 @@ lbtTitle.place(x=55, y=25)
 
 #Добавление кнопки и метки для графика 1
 
-btnChart = tk.Button(window, text = "График 1", font = ('Helvetica',10, 'bold'))
-btnChart.place(x=40, y=115, width=90, height=30)
+btnChart1 = tk.Button(window, text = "График 1", font = ('Helvetica',10, 'bold'), command=chart1.plot_chart)
+btnChart1.place(x=40, y=115, width=90, height=30)
 
-lbtChart = tk.Label(text = "График синуса matplotlib")
-lbtChart.place(x=170, y=122)
+lbtChart1 = tk.Label(text = "График синуса matplotlib")
+lbtChart1.place(x=170, y=122)
+
+#Добавление кнопки и метки для графика 2
+
+btnChart2 = tk.Button(window, text = "График 2", font = ('Helvetica',10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=165, width=90, height=30)
+
+lbtChart2 = tk.Label(text = "Нормальное распределение")
+lbtChart2.place(x=170, y=172)
 
 
 #Добавление кнопки закрытия программы
