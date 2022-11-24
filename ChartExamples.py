847b7687f61f8b8bb66b091c2d1dab8540efd3e6
nutoss <113785849+nutoss@users.noreply.github.com>
2022-11-24 10:22:50 +0300

    Добавление групп кнопка-метка

--- ChartExamples.py
+++ ChartExamples.py
@@ -13,7 +13,7 @@ def do_close():
 
 #Создание главного окна
 window=tk.Tk()
-window.geometry("450x450")
+window.geometry("450x550")
 window.title ("Примеры построения графиков")
 
 #Добавление метки заголовка
@@ -36,10 +36,50 @@ btnChart2.place(x=40, y=165, width=90, height=30)
 lbtChart2 = tk.Label(text = "Нормальное распределение")
 lbtChart2.place(x=170, y=172)
 
+#Добавление кнопки и метки для графика 3
+
+btnChart2 = tk.Button(window, text = "График ", font = ('Helvetica',10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=215, width=90, height=30)
+
+lbtChart2 = tk.Label(text = "Нормальное распределение - 3 графика")
+lbtChart2.place(x=170, y=222)
+
+#Добавление кнопки и метки для графика 4
+
+btnChart2 = tk.Button(window, text = "График 4", font = ('Helvetica',10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=265, width=90, height=30)
+
+lbtChart2 = tk.Label(text = "Описание графика")
+lbtChart2.place(x=170, y=272)
+
+
+#Добавление кнопки и метки для графика 5
+
+btnChart2 = tk.Button(window, text = "График 5", font = ('Helvetica',10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=315, width=90, height=30)
+
+lbtChart2 = tk.Label(text = "Описание графика")
+lbtChart2.place(x=170, y=322)
+
+#Добавление кнопки и метки для графика 6
+
+btnChart2 = tk.Button(window, text = "График 6", font = ('Helvetica',10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=365, width=90, height=30)
+
+lbtChart2 = tk.Label(text = "Описание графика")
+lbtChart2.place(x=170, y=372)
+
+#Добавление кнопки и метки для графика 7
+
+btnChart2 = tk.Button(window, text = "График 7", font = ('Helvetica',10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=415, width=90, height=30)
+
+lbtChart2 = tk.Label(text = "Описание графика")
+lbtChart2.place(x=170, y=422)
 
 #Добавление кнопки закрытия программы
 btnClose = tk.Button(window, text = "Закрыть", font = ('Helvetica',10, 'bold'), command=do_close)
-btnClose.place(x=330, y=400, width=90, height=30)
+btnClose.place(x=330, y=500, width=90, height=30)
 
 
 
